e132b1f29baa46329357b061b9b54a0cf2e87d5f
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-06-28 02:31:28 +0100

    app updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/collosseum.png" },
+  { "src": "/img/eiffle.jpg" },
+  { "src": "/img/itza,jpg" },
+  { "src": "/img/janeiro.jpg" },
+  { "src": "/img/pyramid.jpg" },
+  { "src": "/img/statue.jpg" }
+]
+
 function App() {
+
+  // shuffle cards 
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random() }))
+  }
+
   return (
     <div className="App">
-      <h1>Magic Match Card</h1>
-      <button>Start New Game</button>
+      <h1> Card Puzzle</h1>
+      <button>New Game</button>
     </div>
   );
 }
