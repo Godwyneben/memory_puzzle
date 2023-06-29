c7d8321b9bcbb1ec4380dfcbc06171e2ee74c3c2
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-06-29 02:10:29 +0100

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-export default function App() {
+import './App.css'
+
+const cardImages = [
+  { "src": "/img/collesseum.jpg" },
+  { "src": "/img/eiffle.jpg" },
+  { "src": "/img/itza.jpg" },
+  { "src": "/img/janeiro.jpg" },
+  { "src": "/img/pyramid.jpg" },
+  { "src": "/img/statue.jpg" }
+]
+
+
+ function App() {
+ 
+  // shuffle cards 
+  const shuffleCards = () => {
+    const shuffledCards 
+  }
+
+
   return (
-    <div>
+    <div className='App'>
+      <h1>Card Puzzle</h1>
+      <button>New Game</button>
       
     </div>
   )
 }
+
+export default App
